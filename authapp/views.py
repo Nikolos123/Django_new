dd0084159f8cb44a84f519ad8c4a5dcc7c6f6567
niko <isp06@mail.ru>
2021-03-05 23:40:18 +0300

    lesson-4 auth

--- authapp/views.py
+++ authapp/views.py
@@ -1,20 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponsePermanentRedirect
+from django.contrib import auth,messages
+from django.urls import reverse
+from authapp.forms import  UserCreationForm,UserLoginForm
 
 from authapp.models import  User
 
 # функцияя = вьюхи = контролеры.
 def login(request):
+
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username = username , password = password)
+            if user and user.is_active:
+                auth.login(request,user)
+                return HttpResponsePermanentRedirect(reverse('index'))
+    else:
+        form = UserLoginForm
     context = {
-        'title': 'GeekShop - Авторизация',
-        # 'header': 'Добро пожаловать',
-        # 'username': 'Иванов Иван',
-    }
+            'title': 'GeekShop - Авторизация',
+            'form': form,
+        }
     return render(request, 'authapp/login.html', context)
 
 def register(request):
     context = {
         'title': 'GeekShop - Регистрация',
-        # 'header': 'Добро пожаловать',
-        # 'username': 'Иванов Иван',
     }
     return render(request, 'authapp/register.html', context)
