b0a79b144e1e4a4d2e047522bbc63e6ed1cbeea1
niko <isp06@mail.ru>
2021-03-14 11:30:09 +0300

    lesson-7 creat controller admin-users

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from authapp.models import  User
 
 def index(request):
-    return render(request,'')
+    return render(request,'index.html')
+
+ #READ
+def admin_users(request):
+    context = {'users':User.objects.all()}
+    return render(request,'admin-users-read.html',context)
+#CREAT
+def admin_users_create(request):
+    return render(request,'admin-users-creat.html')
+
+#UPDATE
+def admin_users_update(request):
+    return render(request,'admin-users-update-delete.html')
+
+#DELETE
+def admin_users_delete(request):
+  pass
