ebc08505d36aa1fab5858212533fd514a7de116d
niko <isp06@mail.ru>
2021-03-16 13:41:52 +0300

    lesson-7 added admin

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,22 +1,44 @@
-from django.shortcuts import render
+from re import error
 
-from authapp.models import  User
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from authapp.models import User
+from adminapp.forms import UserAdminRegistrationForm
+
 
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
 
- #READ
+
+# READ
 def admin_users(request):
-    context = {'users':User.objects.all()}
-    return render(request,'admin-users-read.html',context)
-#CREAT
+    context = {'users': User.objects.all()}
+    return render(request, 'admin-users-read.html', context)
+
+
+# CREAT
 def admin_users_create(request):
-    return render(request,'admin-users-creat.html')
+    if request.method == 'POST':
+        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admin_staff:admin_users'))
+        else:
+            print(error)
+    else:
+        form = UserAdminRegistrationForm()
+    context = {
+        'title': 'GeekShop - Регистрация',
+        'form': form,
+    }
+    return render(request, 'admin-users-creat.html', context)
 
-#UPDATE
+
+# UPDATE
 def admin_users_update(request):
-    return render(request,'admin-users-update-delete.html')
+    return render(request, 'admin-users-update-delete.html')
 
-#DELETE
+
+# DELETE
 def admin_users_delete(request):
-  pass
+    pass
