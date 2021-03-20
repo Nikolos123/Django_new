8d6fb39a4ca2ccde718d08f1f7abdb9cd225d9f9
Nikolos123 <Isponec2>
2021-03-20 14:40:32 +0300

    added admin django panel

--- basketapp/admin.py
+++ basketapp/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from basketapp.models import Basket
 # Register your models here.
-admin.site.register(Basket)
+# admin.site.register(Basket)
+
+
+@admin.register(Basket)
+class BasketAdmin(admin.ModelAdmin):
+    list_display = ('product','quantity')
+    fields = (('product','quantity'),)
+    readonly_fields = ('product',)
+    ordering = ('product',)
+    search_fields = ('product',)
