7b942f11f129aa2ba395a49395bd8fd4a6440217
Nikolos123 <Isponec2>
2021-04-17 12:53:04 +0300

    update setting adn urls geekshop

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -116,18 +116,12 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'Название вашей базы данных',
-        # 'USER': 'Ваш пользователь БД',
-        # 'PASSWORD': 'Ваше пароль',
-        # 'HOST': 'localhost',
-        # 'PORT': '3306',
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 DATABESE_SECRETS_FILE = "geekshop/database.json"
 DATABESE = {}
