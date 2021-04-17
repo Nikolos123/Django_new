c192a7bd1f139d88821fed93f7c3f55cafb60379
Nikolos123 <Isponec2>
2021-04-17 15:47:56 +0300

    update setting and edded debug panel

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm0y-s2nhj(*ba33vbhlgg#b10i4uv0$qd*^-07(bw*f&voxtdw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -41,7 +41,10 @@ INSTALLED_APPS = [
     'basketapp',
     'adminapp',
     'ordersapp',
-    'social_django'
+    'social_django',
+
+    'debug_toolbar',
+    'template_profiler_panel',
 
 ]
 
@@ -89,6 +92,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
     'social_django.middleware.SocialAuthExceptionMiddleware',
+
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
 ]
 
 ROOT_URLCONF = 'geekshop.urls'
@@ -115,28 +120,27 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-DATABESE_SECRETS_FILE = "geekshop/database.json"
-DATABESE = {}
-if os.path.exists(DATABESE_SECRETS_FILE):
-    with open(DATABESE_SECRETS_FILE, 'r')as f:
-        DATABESE = json.load(f)
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': DATABESE.get('ENGINE', ""),
-        'NAME': DATABESE.get('NAME', ""),
-        'USER': DATABESE.get('USER', "")
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABESE_SECRETS_FILE = "geekshop/database.json"
+    DATABESE = {}
+    if os.path.exists(DATABESE_SECRETS_FILE):
+        with open(DATABESE_SECRETS_FILE, 'r')as f:
+            DATABESE = json.load(f)
+
+    DATABASES = {
+        'default': {
+            'ENGINE': DATABESE.get('ENGINE', ""),
+            'NAME': DATABESE.get('NAME', ""),
+            'USER': DATABESE.get('USER', "")
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -175,7 +179,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
 
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
 
@@ -211,3 +214,29 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 )
+
+if DEBUG:
+    def show_toolbar(request):
+        return True
+
+
+    DEBUG_TOOLBAR_CONFIG = {
+        'SHOW_TOOLBAR_CALLBACK': show_toolbar,
+    }
+
+    DEBUG_TOOLBAR_PANELS = [
+        'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        'debug_toolbar.panels.settings.SettingsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
+        'debug_toolbar.panels.logging.LoggingPanel',
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+        'debug_toolbar.panels.profiling.ProfilingPanel',
+        'template_profiler_panel.panels.template.TemplateProfilerPanel',
+    ]
